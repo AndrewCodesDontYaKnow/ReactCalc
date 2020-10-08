5539b5c29dd31d433aff9bed5887b54eca1ceeb1
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-10-08 16:15:00 -0500

    created basic calculator

--- reactcalc/src/App.js
+++ reactcalc/src/App.js
@@ -1,14 +1,141 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import ClearButton from "./components/ClearButton";
 
 class App extends Component {
-render() {
-  return (
-    <div className="App">test
-    </div>
-  );
-}
+  // container for holding the states
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: "",
+      previousNumber: "",
+      currentNumber: "",
+      operator: "",
+    };
+  }
+
+  addToInput = (val) => {
+    this.setState({ input: this.state.input + val });
+  };
+
+  addZeroToInput = (val) => {
+    // if this.state.input is not empty then add zero
+    if (this.state.input !== "") {
+      this.setState({ input: this.state.input + val });
+    }
+  };
+
+  addDecimal = (val) => {
+    // if there is no decimal in input, then add the decimal
+    if (this.state.input.indexOf(".") === -1) {
+      this.setState({ input: this.state.input + val });
+    }
+  };
+
+  clearInput = () => {
+    this.setState({ input: "" });
+  };
+
+  add = () => {
+    // store the previous number so we can use it in the operator function
+    this.state.previousNumber = this.state.input;
+    this.setState({ input: "" });
+    this.state.operator = "plus";
+  };
+
+  evaluate = () => {
+    this.state.currentNumber = this.state.input;
+    if (this.state.operator == "plus") {
+      this.setState({
+        input:
+          parseInt(this.state.previousNumber) +
+          parseInt(this.state.currentNumber),
+      });
+    } else if (this.state.operator == "subtract") {
+      this.setState({
+        input:
+          parseInt(this.state.previousNumber) -
+          parseInt(this.state.currentNumber),
+      });
+    } else if (this.state.operator == "multiply") {
+      this.setState({
+        input:
+          parseInt(this.state.previousNumber) *
+          parseInt(this.state.currentNumber),
+      });
+    } else if (this.state.operator == "divide") {
+      this.setState({
+        input:
+          parseInt(this.state.previousNumber) /
+          parseInt(this.state.currentNumber),
+      });
+    }
+  };
+
+  subtract = () => {
+    this.state.previousNumber = this.state.input;
+    this.setState({ input: "" });
+    this.state.operator = "subtract";
+  };
+
+  multiply = () => {
+    this.state.previousNumber = this.state.input;
+    this.setState({ input: "" });
+    this.state.operator = "multiply";
+  };
+
+  divide = () => {
+    this.state.previousNumber = this.state.input;
+    this.setState({ input: "" });
+    this.state.operator = "divide";
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="calc-wrapper">
+          <div className="row">
+            <Input>{this.state.input}</Input>
+          </div>
+
+          <div className="row">
+            <Button handleClick={this.addToInput}>7</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
+            <Button handleClick={this.divide}>/</Button>
+          </div>
+
+          <div className="row">
+            <Button handleClick={this.addToInput}>4</Button>
+            <Button handleClick={this.addToInput}>5</Button>
+            <Button handleClick={this.addToInput}>6</Button>
+            <Button handleClick={this.multiply}>*</Button>
+          </div>
+
+          <div className="row">
+            <Button handleClick={this.addToInput}>1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
+            <Button handleClick={this.add}>+</Button>
+          </div>
+
+          <div className="row">
+            <Button handleClick={this.addDecimal}>.</Button>
+            <Button handleClick={this.addZeroToInput}>0</Button>
+            <Button handleClick={this.evaluate}>=</Button>
+            <Button handleClick={this.subtract}>-</Button>
+          </div>
+
+          <div className="row">
+            <ClearButton handleClear={this.clearInput}>Clear</ClearButton>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ reactcalc/src/components/Button.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./Button.css";
+
+class Button extends Component {
+  // returns true if val is a number, a period, or equals sign
+  isOperator = (val) => {
+    return !isNaN(val) || val === "." || val === "=";
+  };
+
+  render() {
+    return (
+      <div
+        // if the button's val is a number, dont add a class, if it's an operator, add the class 'operator'
+        className={`button ${
+          this.isOperator(this.props.children) ? "" : "operator"}`}
+          onClick={() => this.props.handleClick
+            (this.props.children)}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default Button;
