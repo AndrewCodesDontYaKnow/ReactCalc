6bc11ce4bbf1184753e5cbcefecae2310e666c75
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-10-11 15:38:00 -0500

    Updates

---
+++ precache-manifest.97ae7de5e0df6998a61c12fe0cfd8b44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc8e5e800e82f2611683a81ddcbcf47",
+    "url": "/ReactCalc/index.html"
+  },
+  {
+    "revision": "c27661b7624378e1cb30",
+    "url": "/ReactCalc/static/css/main.d9882f86.chunk.css"
+  },
+  {
+    "revision": "e4257c7627302049b272",
+    "url": "/ReactCalc/static/js/2.10606916.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactCalc/static/js/2.10606916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27661b7624378e1cb30",
+    "url": "/ReactCalc/static/js/main.3569e289.chunk.js"
+  },
+  {
+    "revision": "d1a9b43e60abdc09ef1e",
+    "url": "/ReactCalc/static/js/runtime-main.e9aed97b.js"
+  }
+]);
