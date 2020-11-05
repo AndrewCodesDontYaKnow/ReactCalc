ea87e8e09fa9fb202b4c5dc7224271556717aeeb
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-11-04 19:43:32 -0600

    removed mongoose code from server and api

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const morgan = require('morgan');
-const path = require('path');
+// const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const routes = require('./routes/api')
 
-mongoose.connect('mongodb://localhost/reactcalc', {
-     useNewUrlParser: true,
-     useUnifiedTopology: true 
-    });
-
-mongoose.connection.on('connected', () => {
-    console.log('Mongoose is connected!')
-})
 
 // HTTP request logger
 app.use(morgan('tiny'));
