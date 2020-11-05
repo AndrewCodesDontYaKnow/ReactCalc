dd1f67e03e9c436b27ea81d981f8210746ed3823
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-11-04 20:00:43 -0600

    installed dependencies for sql db

---
+++ server_old.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+// const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const routes = require('./routes/api')
+
+
+// HTTP request logger
+app.use(morgan('tiny'));
+app.use('/api', routes)
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`)
+});
