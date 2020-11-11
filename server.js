97143b6d66e6b99beaeeefac33a4bbedad5548b3
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-11-11 11:35:19 -0600

    changed sql queries to match table

--- server.js
+++ server.js
@@ -11,15 +11,17 @@ app.use(express.static(path.join(__dirname, "/client/build")));
 // This route serves the React app
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "/client/build", "index.html")));
 
-const SELECT_TEN_CALCS_QUERY = 'SELECT calc FROM calctable ORDER BY id DESC LIMIT 10;';
-const CLEAR_ALL_CALCS_QUERY = 'DELETE * FROM calctable;'
+const SELECT_TEN_CALCS_QUERY = 'SELECT calc FROM calculations ORDER BY id DESC LIMIT 10;';
+const CLEAR_ALL_CALCS_QUERY = 'DELETE * FROM calculations;'
+const INSERT_CALCULATION_QUERY = `INSERT INTO calculations (calc) VALUES ('${calc}');`;
+
 // const SELECT_ALL_CALCULATIONS_QUERY = 'SELECT * FROM calctable';
 
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
-    database: 'calculator_db'
+    database: 'rapqabntephwgrnb'
 });
 
 if (process.env.JAWSDB_URL) {
@@ -29,7 +31,7 @@ if (process.env.JAWSDB_URL) {
         host: 'localhost',
         user: 'root',
         password: 'password',
-        database: 'calculator_db'
+        database: 'rapqabntephwgrnb'
     })
 }
 
@@ -48,8 +50,6 @@ app.use(cors());
 app.get('/calculations/add', (req, res) => {
     const { calc } = req.query;
     console.log(`here is req.query.calc: ${calc}`)
-
-    const INSERT_CALCULATION_QUERY = `INSERT INTO calctable (calc) VALUES ('${calc}');`;
     
     connection.query(INSERT_CALCULATION_QUERY, (err, results) => {
         if(err) {
